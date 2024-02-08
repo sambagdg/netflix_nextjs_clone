34b83fcca4c2c7733d63ea3e4bb4e2137b757f40
Samba Gandega <sambagdg@gmail.com>
2024-02-08 19:31:20 +0100

    prisma updating

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import { redirect } from 'next/navigation'
 
 export default async function Home() {
   const session = await getServerSession(authOptions)
-  if (session) {
+  if (!session) {
     return redirect("/home");
   }
   else {
